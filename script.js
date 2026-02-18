c17d3dec6de37b4fa178d67260ba9d5fa9d53c6b
Maria Eduarda Pacheco Delazari <m.delazari.dev@gmail.com>
2026-02-18 08:59:12 -0300

    Array that will store the tasks.

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 const inputElement = document.querySelector("#task-input");
 const buttonElement = document.querySelector("#add-task-button");
 const listElement = document.querySelector("#task-list");
+
+/* Array que vai armazenar as tarefas */
+let tasks = [];
