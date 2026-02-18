11732645a446c8c25ad861783b390d369271af2e
Maria Eduarda Pacheco Delazari <m.delazari.dev@gmail.com>
2026-02-18 08:58:17 -0300

    Selection of DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+
+/*Seleção dos elementos do DOM */
+const inputElement = document.querySelector("#task-input");
+const buttonElement = document.querySelector("#add-task-button");
+const listElement = document.querySelector("#task-list");
